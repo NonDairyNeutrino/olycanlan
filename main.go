da1feb1219ac532e3c330c0efe65971a21987e22
Natalie Chapman <nate@nathanwchapman.com>
2026-05-15 12:32:06 -0700

    first compilation!

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+)
 
 func main() {
-    discord, err := discordgo.New("Bot " + "authentication token")
+	discord, err := discordgo.New("Bot " + "authentication token")
+
+	fmt.Println("discord = ", discord)
+	fmt.Println("err = ", err)
+
+	// if err != nil {
+	// 	fmt.Println("Error connecting to Discord (Is the auth token correct?)", err)
+	// } else {
+	// 	fmt.Println("Discord connected:", discord)
+	// }
+
 }
