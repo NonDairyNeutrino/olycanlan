db39d71ab3f52a0dbdf7822f53fb9cde58209a43
Natalie Chapman <nate@nathanwchapman.com>
2026-05-16 19:18:17 -0700

    add joho/godotenv dependency

--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	discord, err := discordgo.New("Bot " + "authentication token")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	fmt.Println("discord = ", discord)
-	fmt.Println("err = ", err)
+	discord, err := discordgo.New("Bot " + os.Getenv("APP_ID"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// if err != nil {
-	// 	fmt.Println("Error connecting to Discord (Is the auth token correct?)", err)
-	// } else {
-	// 	fmt.Println("Discord connected:", discord)
-	// }
+	fmt.Println("ready?", discord.DataReady)
 
 }
