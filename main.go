8fd18d203ccd1971d6168aa0e7711b373a522e2b
Natalie Chapman <nate@nathanwchapman.com>
2026-05-18 07:55:11 -0700

    add match reporting psuedocode

--- main.go
+++ main.go
@@ -8,6 +8,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Match Recording
+// input: channel
+// output: table of current scores
+// 1. Scan channel for posts since last scan
+// 2. For each post
+//   2.1 test post is a "report post"
+// 2. For each report post
+//   2.1 Extract players
+//   2.2 Extract scores
+//   2.3 Extract match type e.g. bounty, non-bounty first, non-bounty repeated
+//   2.4 Return "Report" with above info
+// 3. For each player
+//   3.1 Calculate new points for player based on Reports
+// 4. Update Discord scoreboard post with new and total points
+// 5. Update website scoreboard with new and total points
+
 func main() {
 	// start by loading things like API tokens from the .env file
 	err := godotenv.Load()
